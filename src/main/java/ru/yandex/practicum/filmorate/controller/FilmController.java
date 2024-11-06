9503e22f50d92b1785e350640a08c8b829c0a5cc
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-06 21:15:16 +0300

    Release create, update and get methods in FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,69 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        log.info("Добавление нового фильма: {}", film.getName());
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film newFilm) {
+
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            log.info("Обновление фильма: {}", oldFilm.getName());
+
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+
+            return oldFilm;
+        } else {
+            log.error("Фильм с id = " + newFilm.getId() + " не найден");
+            throw new FilmNotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,40 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NegativeOrZero;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
+import ru.yandex.practicum.filmorate.validator.ReleaseDateCheck;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Film {
+    @NotNull(message = "Id должен быть указан")
+    Long id;
+
+    @NotNull(message = "Название не может быть пустым")
+    @NotBlank(message = "Название не может состоять из пробелов")
+    String name;
+
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    String description;
+
+    @ReleaseDateCheck
+    LocalDate releaseDate;
+
+    @NotNull(message = "Продолжительность фильма должна быть положительным числом")
+    @PositiveOrZero(message = "Продолжительность фильма должна быть положительным числом")
+    Integer duration;
 }
