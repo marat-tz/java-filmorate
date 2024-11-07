9da8f43b1fa95d871038e311b84840b853d98d12
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-07 20:08:43 +0300

    Fix NotSpacesValidator

--- src/main/java/ru/yandex/practicum/filmorate/validator/NotSpacesValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/NotSpacesValidator.java
@@ -8,6 +8,6 @@ import java.time.LocalDate;
 public class NotSpacesValidator implements ConstraintValidator<NotSpaces, String> {
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return s.matches(".*\\s.*");
+        return !s.contains(" ");
     }
 }
