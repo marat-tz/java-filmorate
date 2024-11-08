a8d926bdd9b2d350a7bcbed4a82248e6fbf9d24b
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-08 21:35:08 +0300

    Fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/validator/NotSpacesValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/NotSpacesValidator.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.validator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.time.LocalDate;
-
 public class NotSpacesValidator implements ConstraintValidator<NotSpaces, String> {
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
