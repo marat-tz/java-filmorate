e7ac6afb7a2d637a5e426d7dac818322b0e998ec
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-08 19:57:09 +0300

    Add Gson. Add LocalDateAdapter

---
+++ src/main/java/ru/yandex/practicum/filmorate/adapters/LocalDateAdapter.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.filmorate.adapters;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import org.springframework.format.datetime.DateFormatter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Objects;
+
+public class LocalDateAdapter extends TypeAdapter<LocalDate> {
+
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
+
+    @Override
+    public void write(JsonWriter jsonWriter, LocalDate value) throws IOException {
+        if (Objects.isNull(value)) {
+            jsonWriter.value("null");
+        } else {
+            jsonWriter.value(value.format(dtf));
+        }
+    }
+
+    @Override
+    public LocalDate read(JsonReader jsonReader) throws IOException {
+        LocalDate date;
+        try {
+            date = LocalDate.parse(jsonReader.nextString(), dtf);
+        } catch (DateTimeParseException ex) {
+            date = null;
+        }
+        return date;
+    }
+}
