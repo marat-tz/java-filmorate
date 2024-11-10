d0b58a55a2a20140dc2f482f1f1d51745a492abe
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-10 21:24:26 +0300

    Fix codestyle

--- src/main/java/ru/yandex/practicum/filmorate/mappers/FilmMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/FilmMapper.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.mappers;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 @Mapper(componentModel = "spring")
 public interface FilmMapper {
