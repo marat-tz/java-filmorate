5e7d3dd5b2238374f58742ce4ac1349ddfd17a14
Marat Tazhetdinov <unlim089@yandex.ru>
2024-11-19 22:36:18 +0300

    Separate storage and handling logic from FilmController and UserController classes to InMemory...Storage classes

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,66 +8,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.mappers.UserMapper;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
-@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final Map<Long, User> users = new HashMap<>();
-
     @Autowired
-    private UserMapper mapper;
+    private UserStorage storage;
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return storage.findAll();
     }
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        log.info("Создание нового пользователя: {}", user.getLogin());
-        if (Objects.isNull(user.getName())) {
-            user = mapper.toUserIfNoName(user);
-        } else {
-            user = user.toBuilder()
-                    .id(getNextId())
-                    .build();
-        }
-        users.put(user.getId(), user);
-        log.info("Пользователь c id = {} успешно добавлен", user.getId());
-        return user;
+        return storage.create(user);
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User newUser) {
-        Long id = newUser.getId();
-        if (users.containsKey(id)) {
-            log.info("Обновление данных пользователя с id = {}", id);
-            User user = mapper.toUser(newUser);
-            users.put(id, user);
-            log.info("Пользователь с id = {} успешно обновлён", id);
-            return user;
-        } else {
-            log.error("Пользователь с id = {} не найден", id);
-            throw new ValidationException("Пользователь с id = " + id + " не найден");
-        }
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return storage.update(newUser);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.mappers.FilmMapper;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+
+    private final Map<Long, Film> films = new HashMap<>();
+    private final FilmMapper mapper;
+
+    @Autowired
+    public InMemoryFilmStorage(FilmMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @Override
+    public Film create(Film film) {
+        log.info("Добавление нового фильма: {}", film.getName());
+        film = film.toBuilder().id(getNextId()).build();
+        films.put(film.getId(), film);
+        log.info("Фильм c id = {} успешно добавлен", film.getId());
+        return film;
+    }
+
+    @Override
+    public Film update(Film newFilm) {
+        Long id = newFilm.getId();
+        Film film;
+        if (films.containsKey(id)) {
+            log.info("Обновление фильма с id = {}", id);
+            film = mapper.toFilm(newFilm);
+            films.put(id, film);
+            log.info("Фильм с id = {} успешно обновлён", id);
+            return film;
+        } else {
+            log.error("Фильм с id = {} не найден", id);
+            throw new ValidationException("Фильм с id = " + id + " не найден");
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
