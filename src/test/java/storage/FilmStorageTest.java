c8849165113f128969777e3ca2b199845c290f0b
Marat Tazhetdinov <unlim089@yandex.ru>
2025-01-15 22:39:27 +0300

    Добавить тесты метода getPopularFilms

--- src/test/java/storage/FilmStorageTest.java
+++ src/test/java/storage/FilmStorageTest.java
@@ -1,4 +1,4 @@
-package controller;
+package storage;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,25 +9,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.FilmorateApplication;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.MpaStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = FilmorateApplication.class)
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class FilmControllerTest {
+public class FilmStorageTest {
 
     private final JdbcTemplate jdbcTemplate;
     private final UserStorage userStorage;
     private final FilmStorage filmStorage;
+
+    private final MpaStorage mpaStorage;
 
     @BeforeEach
     public void beforeEach() {
@@ -39,8 +40,13 @@ public class FilmControllerTest {
                 .name("test")
                 .birthday(LocalDate.of(1995, 10, 10))
                 .build();
-
         userStorage.create(user);
+
+        Mpa mpa = Mpa
+                .builder()
+                .id(1L)
+                .name("test")
+                .build();
 
         Film film = Film
                 .builder()
@@ -49,21 +55,15 @@ public class FilmControllerTest {
                 .description("test desc")
                 .releaseDate(LocalDate.of(1995, 10, 10))
                 .duration(100)
+                .mpa(mpa)
                 .build();
-
         filmStorage.create(film);
     }
 
-//    @AfterEach
-//    public void afterEach() {
-//        jdbcTemplate.update(FileReader.readString("src/test/resources/drop.sql"));
-//    }
-
     @Test
-    void testFindUserById() {
-        Optional<User> userOptional = userStorage.findById(1L);
-
-        assertTrue(userOptional.isPresent());
+    void findById_shouldFindFilmById() {
+        Film film = filmStorage.findById(1L);
+        assertEquals(1L, film.getId());
     }
 
 }
