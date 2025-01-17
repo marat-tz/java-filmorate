977a603def285de481d1ea4a279348f5ea1c9457
Marat Tazhetdinov <unlim089@yandex.ru>
2025-01-17 19:02:53 +0300

    Заменить аннотации на Value

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -2,11 +2,9 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+import lombok.Builder;
+import lombok.Value;
 
-@Getter
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @Value
 @Builder
 public class Review {
