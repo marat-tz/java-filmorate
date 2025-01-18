09032f6f49d936b310f496595405a3b0ed598b07
Marat Tazhetdinov <unlim089@yandex.ru>
2025-01-18 20:57:04 +0300

    Залогировать контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.impl.DbDirectorServiceImpl;
 
 import java.util.Collection;
 
+@Slf4j
 @RestController
 @RequestMapping("/directors")
 public class DirectorController {
@@ -19,30 +21,43 @@ public class DirectorController {
     //GET /directors - Список всех режиссёров
     @GetMapping
     public Collection<Director> findAll() {
-        return service.findAll();
+        log.info("Получен GET запрос /directors");
+        Collection<Director> result = service.findAll();
+        log.info("Отправлен GET ответ {}", result);
+        return result;
     }
 
     //GET /directors/{id}- Получение режиссёра по id
     @GetMapping("/{id}")
     public Director getById(@PathVariable Long id) {
-        return service.getById(id);
+        log.info("Получен GET запрос /directors/{id}");
+        Director result = service.getById(id);
+        log.info("Отправлен GET ответ {}", result);
+        return result;
     }
 
     //POST /directors - Создание режиссёра
     @PostMapping
     public Director create(@Valid @RequestBody Director director) {
-        return service.create(director);
+        log.info("Получен POST запрос /directors");
+        Director result = service.create(director);
+        log.info("Отправлен POST ответ {}", result);
+        return result;
     }
 
     //PUT /directors - Изменение режиссёра
     @PutMapping
     public Director update(@Valid @RequestBody Director director) {
-       return service.update(director);
+        log.info("Получен PUT запрос /directors");
+        Director result = service.update(director);
+        log.info("Отправлен PUT ответ {}", result);
+        return result;
     }
 
     //DELETE /directors/{id} - Удаление режиссёра
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
+        log.info("Получен DELETE запрос /directors/{id}");
         service.delete(id);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,14 +1,17 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.impl.DbMpaServiceImpl;
 
 import java.util.Collection;
 
+@Slf4j
 @RestController
 @RequestMapping("/mpa")
 public class MpaController {
@@ -21,12 +24,18 @@ public class MpaController {
 
     @GetMapping
     public Collection<Mpa> findAll() {
+        log.info("Получен GET запрос /mpa");
+        Collection<Mpa> result = service.findAll();
+        log.info("Отправлен GET ответ {}", result);
         return service.findAll();
     }
 
     @GetMapping("/{id}")
     public Mpa getNameById(@PathVariable Long id) {
-        return service.getNameById(id);
+        log.info("Получен GET запрос /mpa/{}", id);
+        Mpa result = service.getNameById(id);
+        log.info("Отправлен GET ответ {}", result);
+        return result;
     }
 
 }
