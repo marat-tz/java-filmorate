034768ce8b1fab5241237520d42437504dc457b4
Marat Tazhetdinov <unlim089@yandex.ru>
2025-01-23 18:30:28 +0300

    Удалить избыточные логгеры в мапперах

--- src/main/java/ru/yandex/practicum/filmorate/mappers/ReviewRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/ReviewRowMapper.java
@@ -13,25 +13,12 @@ import java.sql.SQLException;
 public class ReviewRowMapper implements RowMapper<Review> {
     @Override
     public Review mapRow(ResultSet rs, int rowNum) throws SQLException {
-        log.info("Начало метода ReviewRowMapper mapRow");
-
         Long reviewId = rs.getLong("id");
-        log.info("Получено значение reviewId={}", reviewId);
-
         String content = rs.getString("content");
-        log.info("Получено значение content={}", content);
-
         Boolean isPositive = rs.getBoolean("is_positive");
-        log.info("Получено значение isPositive={}", isPositive);
-
         Long userId = rs.getLong("user_id");
-        log.info("Получено значение userId={}", userId);
-
         Long filmId = rs.getLong("film_id");
-        log.info("Получено значение filmId={}", filmId);
-
         Integer useful = rs.getInt("lik") - rs.getInt("dis");
-        log.info("Получено значение useful={}", useful);
 
         return Review.builder()
                 .reviewId(reviewId)
